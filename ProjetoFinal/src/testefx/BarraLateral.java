69f420bc4f62882ffb1dfa1000f0945cae83be58
Gabriel Lima Luz <gabriel.luz.lima>
2021-06-16 15:23:31 -0300

    barra lateral arrumada , ainda precisa especificar como vai saber qual jogador é pra mudar as cores dos icones

--- ProjetoFinal/src/testefx/BarraLateral.java
+++ ProjetoFinal/src/testefx/BarraLateral.java
@@ -17,6 +17,8 @@ public class BarraLateral {
     }
 
     public void Desenhar(int valores[]){
+
+        //FIXME arrumar a ordem dos icones em relacao aos numeros , ;-;
         //Quadrado
         Rectangle lateralBar = new Rectangle();
         lateralBar.setX(0);
@@ -28,84 +30,84 @@ public class BarraLateral {
         //Textos itens
         // metal
         Text metal = new Text();
-        metal.setText(String.valueOf(valores[0]));
+        metal.setText(String.valueOf(55));//valores[0]
         metal.setX(70);
-        metal.setY(169.5);
+        metal.setY(469.5);//409.5
         metal.setFont(Font.font("Verdana", 20));
         metal.setFill(Color.BLACK);
 
 
         // combutivel
         Text fuel = new Text();
-        fuel.setText(String.valueOf(valores[2]));
+        fuel.setText(String.valueOf(66));//valores[2]
         fuel.setX(70);
-        fuel.setY(229.5);
+        fuel.setY(349.5);
         fuel.setFont(Font.font("Verdana", 20));
         fuel.setFill(Color.BLACK);
 
         //municao
         Text bullet = new Text();
-        bullet.setText(String.valueOf(valores[1]));
-        bullet.setY(289.5);
+        bullet.setText(String.valueOf(77));//valores[1])//;trocar com o combustivel
         bullet.setX(70);
+        bullet.setY(409.5);
         bullet.setFont(Font.font("Verdana", 20));
         bullet.setFill(Color.BLACK);
 
         //guerra
         Text warStarship = new Text();
-        warStarship.setText(String.valueOf(valores[5]));
+        warStarship.setText(String.valueOf(valores[4]));//valores[5]
         warStarship.setX(70);
-        warStarship.setY(349.5);
+        warStarship.setY(169.5 ); //  349.5
         warStarship.setFont(Font.font("Verdana", 20));
         warStarship.setFill(Color.BLACK);
 
         //colonizadora
         Text colonizationStarship = new Text();
-        colonizationStarship.setText(String.valueOf(valores[4]));
+        colonizationStarship.setText(String.valueOf(valores[5]));
         colonizationStarship.setX(70);
-        colonizationStarship.setY(409.5);
+        colonizationStarship.setY(229.5);
         colonizationStarship.setFont(Font.font("Verdana", 20));
         colonizationStarship.setFill(Color.BLACK);
 
         //satelite
         Text satelite = new Text();
         satelite.setText(String.valueOf(valores[3]));
-        satelite.setX(70);
-        satelite.setY(469.5);
+        satelite.setX(70); // 70
+        satelite.setY(289.5); // 469.5
         satelite.setFont(Font.font("Verdana", 20));
         satelite.setFill(Color.BLACK);
 
         //Imagens dos itens
 
         // guerra
-        Image warStarshipImage = new Image("images/naveguerrav.png");
+        Image warStarshipImage = new Image("images/naveguerraa.png");
         ImageView imageViewWarStarship = new ImageView(warStarshipImage);
         imageViewWarStarship.setX(5);
         imageViewWarStarship.setY(139.5);
 
         // colonizadora
-        Image colonizationStarshipImage = new Image("images/navecolonizadorav.png");
+        Image colonizationStarshipImage = new Image("images/navecolonizadoraa.png");
         ImageView imageViewColonizationStarship = new ImageView(colonizationStarshipImage);
         imageViewColonizationStarship.setX(5);
         imageViewColonizationStarship.setY(200.5);
 
         // satelite
-        Image sateliteImage = new Image("images/satelitev.png");
+        Image sateliteImage = new Image("images/satelitea.png");
         ImageView imageViewSatelite = new ImageView(sateliteImage);
         imageViewSatelite.setX(5);
         imageViewSatelite.setY(261.5);
 
-        Image fuelImage = new Image("testefx/nave.png");
+        Image fuelImage = new Image("images/combustivel.png");
         ImageView imageViewFuel = new ImageView(fuelImage);
         imageViewFuel.setX(5);
         imageViewFuel.setY(322.5);
 
-        Image bulletImage = new Image("testefx/nave.png");
+        Image bulletImage = new Image("images/municao.png");
         ImageView imageViewBullet = new ImageView(bulletImage);
         imageViewBullet.setX(5);
         imageViewBullet.setY(383.5);
 
-        Image metalImage = new Image("testefx/nave.png");
+        Image metalImage = new Image("images/metal.png");
         ImageView imageViewMetal = new ImageView(metalImage);
         imageViewMetal.setX(5);
         imageViewMetal.setY(444.5);
